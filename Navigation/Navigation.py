84eaafc3848efca9bc1e103d638a44ee7f276ecd
Michael <m_j_39@hotmail.com>
2026-04-22 13:11:01 +0200

    refracturing

--- Navigation/Navigation.py
+++ Navigation/Navigation.py
@@ -4,16 +4,17 @@ import cv2
 import numpy as np
 from pathlib import Path
 from point import *
-
-import Object_Tracking
-from Object_Tracking.Course_detecter import Find_Arena
-from Object_Tracking.Object_Tracking import find_objects_in_image, draw_detections_on_warp
+from Object_Tracking.Object_Tracking import *
 import MoveBot.MoveBot
 
+#find distance between two points (fx. bot and ball)
+def find_distance_between_points(point1: Point, point2: Point):
+    return np.sqrt(np.square(point2.x - point1.x) + np.square(point2.y - point1.y))
+
+#find the best turn from current heading to a point
 def find_turn(current_heading, point1, point2):
     direction_radian = np.atan2(point2.y - point1.y, point2.x - point1.x)
     target_direction = round(math.degrees(direction_radian))
-    turn_flag = ""
     delta_direction = target_direction - current_heading
 
     # Normalize to [-180, 180]
@@ -31,67 +32,59 @@ def find_turn(current_heading, point1, point2):
 
     return turn_flag, turn_angle
 
-#find distance between two points (fx. bot and ball)
-def distance_between_points(point1: Point, point2: Point):
-    return np.sqrt(np.square(point2.x - point1.x) + np.square(point2.y - point1.y))
-
+#find and set image folder/files
 base_path = Path(__file__).resolve().parent
 images_folder = base_path.parent / "Images"
 image_files = list(images_folder.glob("*.jpg"))
 
+#load image
 img = cv2.imread(image_files[0])
 
-#find bot (start by using x=40,y=40 or something)
-#bot is a circle with diameter 320mm
-#find yellow ball
-#determine angle from bots facing to yellow ball
-#turn towards yellow ball
-#Check angle is correct
-#determine distance from collector to yellow ball
-#drive forward to yellow ball
-#Check position is correct
-
+#picture dimensions in pixel
 WARP_W, WARP_H = 800, 1200
-COURT_W_CM, COURT_H_CM = 120.0, 180.0
-warped = Find_Arena(img, out_w=WARP_W, out_h=WARP_H)
 
+#actual dimensions in cm
+COURT_W_CM, COURT_H_CM = 120.0, 180.0
+
+#find arena in img and draw on warped
+warped = find_arena(img, out_w=WARP_W, out_h=WARP_H)
+
+#finds all objects in img
+orange_ball, white_ball_list, cross = find_objects_in_image(img, WARP_W, WARP_H)
+
+#draw objects on warped
+draw_detections_on_warp(warped, orange_ball, "position", warp_w_px=WARP_W, warp_h_px=WARP_H, court_w_cm=COURT_W_CM, court_h_cm=COURT_H_CM)
 
 #temp variables to simulate bot position and heading
-botX = 40
-botY = 40
-botCoordinates = Object_Tracking.Object_Tracking.world_cm_to_px(botX, botY, WARP_W, WARP_H)
+bot_x = 40
+bot_y = 40
+botCoordinates = world_cm_to_px(bot_x, bot_y, WARP_W, WARP_H)
 currentHeading = 90 #measures from x-axis and goes counter-clockwise
 
-
-Orangeball, whiteballs, cross = find_objects_in_image(img,WARP_W,WARP_H)
-
 #ball coordinates
-orangeX = Orangeball[3][0]
-orangeY = Orangeball[3][1]
-ballCoordinates = Object_Tracking.Object_Tracking.world_cm_to_px(orangeX, orangeY, WARP_W, WARP_H)
-botDimensions = Object_Tracking.Object_Tracking.world_cm_to_px(32, 32, WARP_W, WARP_H)
+orange_x = orange_ball[3][0]
+orange_y = orange_ball[3][1]
 
-def drawBot(x, y):
-    radius = Object_Tracking.Object_Tracking.radius_cm_to_px(16)
-    return cv2.circle(warped, botCoordinates, radius, (0, 0, 255), 3)
+#convert from cm to pixel
+ballCoordinates = world_cm_to_px(orange_x, orange_y, WARP_W, WARP_H)
+botDimensions = world_cm_to_px(32, 32, WARP_W, WARP_H)
 
+#draw bot on warped (current bot radius is 16)
+warped = cv2.circle(warped, botCoordinates, radius_cm_to_px(16), (0, 0, 255), 3)
 
-visual = warped.copy()
-draw_detections_on_warp(visual,Orangeball,"position",warp_w_px=WARP_W,warp_h_px=WARP_H,court_w_cm=COURT_W_CM,court_h_cm=COURT_H_CM)
-output_folder = ""
-output_path = output_folder + "visTest.jpg"
-cv2.imwrite(output_path, visual)
+#draw arrow on warped
+warped = cv2.arrowedLine(warped, botCoordinates, ballCoordinates, (0,255,0), 3)
 
-
-
-
-
-warped = drawBot(botX, botY)
-warped = cv2.arrowedLine(warped, botCoordinates, ballCoordinates, (0,255,0), 3)                 # Read image
+#resize and show window with picture: warped
 warped = cv2.resize(warped, (400, 600))
 cv2.imshow("arrow", warped)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#save picture: warped
+output_folder = ""
+output_path = output_folder + "visTest.jpg"
+cv2.imwrite(output_path, warped)
 
 
 #For testing with actual bot
