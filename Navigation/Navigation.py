f92aec1fff1cdb7e27ac1972b4892b9c824900e0
Michael <m_j_39@hotmail.com>
2026-04-22 10:53:59 +0200

    drawBot now correctly draws the bot.

--- Navigation/Navigation.py
+++ Navigation/Navigation.py
@@ -18,6 +18,7 @@ image_files = list(images_folder.glob("*.jpg"))
 img = cv2.imread(image_files[0])
 
 #find bot (start by using x=40,y=40 or something)
+#bot is a circle with diameter 320mm
 #find yellow ball
 #determine angle from bots facing to yellow ball
 #turn towards yellow ball
@@ -34,11 +35,8 @@ warped = Find_Arena(img, out_w=WARP_W, out_h=WARP_H)
 #temp variables to simulate bot position and heading
 botX = 40
 botY = 40
-#botCoordinates = (botX,botY)
-currentHeading = 90
-
 botCoordinates = Object_Tracking.Object_Tracking.world_cm_to_px(botX, botY, WARP_W, WARP_H)
-
+currentHeading = 90
 
 Orangeball, whiteballs, cross = find_objects_in_image(img,WARP_W,WARP_H)
 
@@ -46,6 +44,12 @@ Orangeball, whiteballs, cross = find_objects_in_image(img,WARP_W,WARP_H)
 orangeX = Orangeball[3][0]
 orangeY = Orangeball[3][1]
 ballCoordinates = Object_Tracking.Object_Tracking.world_cm_to_px(orangeX, orangeY, WARP_W, WARP_H)
+botDimensions = Object_Tracking.Object_Tracking.world_cm_to_px(32, 32, WARP_W, WARP_H)
+
+def drawBot(x, y):
+    radius = Object_Tracking.Object_Tracking.radius_cm_to_px(16)
+    return cv2.circle(warped, botCoordinates, radius, (0, 0, 255), 3)
+
 
 visual = warped.copy()
 draw_detections_on_warp(visual,Orangeball,"position",warp_w_px=WARP_W,warp_h_px=WARP_H,court_w_cm=COURT_W_CM,court_h_cm=COURT_H_CM)
@@ -64,6 +68,7 @@ deltaDirection = targetDirection - currentHeading
 # Normalize to [-180, 180]
 delta = (deltaDirection + 180) % 360 - 180
 
+warped = drawBot(botX, botY)
 warped = cv2.arrowedLine(warped, botCoordinates, ballCoordinates, (0,255,0), 3)                 # Read image
 warped = cv2.resize(warped, (400, 600))
 cv2.imshow("arrow", warped)
