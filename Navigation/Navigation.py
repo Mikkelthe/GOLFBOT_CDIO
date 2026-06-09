0908f63fb37e9e846c7afa7e0c377a4f03551b1d
Michael <m_j_39@hotmail.com>
2026-06-09 21:10:28 +0200

    Added misc comments split center constant into px and cm variant changed Point to int with rounding Added heading arrow for bot

--- Navigation/Navigation.py
+++ Navigation/Navigation.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from point import *
 from Object_Tracking.Object_Tracking import *
 
+#kunne vi holde os til px så vidt som muligt og så kun konvertere til cm ved behov?
+#kan ikke se AruCo ved kanten
+#bane/pixel problem?
 
 #find distance between two points (fx. bot and ball)
 def find_distance_between_points(point1: Point, point2: Point):
@@ -31,6 +34,7 @@ def find_turn(current_heading, point1, point2):
 
     return turn_flag, turn_angle
 
+#find the robot position and heading in the picture using an ArUco-marker
 def find_bot(image):
     aruco_dict = cv2.aruco.getPredefinedDictionary(
         cv2.aruco.DICT_4X4_50
@@ -54,14 +58,8 @@ def find_bot(image):
         angle = np.degrees(
             np.arctan2(heading[1], heading[0])
         )
-        #for debugging
-        #print("Center:", center)
-        #print("Orientation:", angle)
     return center, angle
 
-#for debugging
-#image = cv2.imread("arena3.jpg")
-#find_bot(image)
 
 #find and set image folder/files
 base_path = Path(__file__).resolve().parent
@@ -71,14 +69,13 @@ image_files = list(images_folder.glob("*.jpg"))
 #load image
 img = cv2.imread("arena4.jpg")
 
-#picture dimensions in pixel
+#picture dimensions center in pixel
 WARP_W, WARP_H = 1100, 700
+CENTER_POINT_WARP = Point(WARP_W / 2, WARP_H / 2)
 
-#Center of image in pixel
-center_point = Point(WARP_W / 2, WARP_H / 2)
-
-#actual dimensions in cm
+#actual dimensions and center in cm
 COURT_W_CM, COURT_H_CM = 170.0, 125.0
+CENTER_POINT_CM = Point(COURT_W_CM / 2, COURT_H_CM / 2)
 
 #find arena in img and draw on warped
 warped = find_arena(img, out_w=WARP_W, out_h=WARP_H)
@@ -92,8 +89,7 @@ draw_detections_on_warp(warped, white_ball, "position", warp_w_px=WARP_W, warp_h
 
 #heading measures from x-axis and goes counter-clockwise
 botCoordinates, currentHeading = find_bot(warped)
-#botCoordinates = tuple(botCoordinates.astype(int))
-
+#botCoordinates = botCoordinates.x, botCoordinates.y
 
 #ball coordinates
 white_x = white_ball[0][0]
@@ -110,8 +106,7 @@ bot_height_cm = 46
 bot_height_px = cm_to_px(bot_height_cm)
 ratio_height = cam_height_px / (cam_height_px - bot_height_px)
 
-
-center_to_bot_dist = find_distance_between_points(center_point,botCoordinates)
+center_to_bot_dist = find_distance_between_points(CENTER_POINT_WARP, botCoordinates)
 x_ratio = int(botCoordinates.x / ratio_height)
 y_ratio = int(botCoordinates.y / ratio_height)
 botCoordinates = x_ratio, y_ratio
@@ -120,12 +115,20 @@ botCoordinates = x_ratio, y_ratio
 #draw bot on warped (current bot radius is 16)
 warped = cv2.circle(warped, botCoordinates, cm_to_px(16), (0, 0, 255), 3)
 
-#draw arrow on warped
+#draw current heading from bot on warped
+arrow_length = 100 #px
+end_point = (
+    int(x_ratio + arrow_length * np.cos(np.radians(currentHeading))),
+    int(y_ratio + arrow_length * np.sin(np.radians(currentHeading)))
+)
+warped = cv2.arrowedLine(warped, botCoordinates, end_point, (0,0,255), 3)
+
+#draw arrow from center of bot to center of ball on warped
 warped = cv2.arrowedLine(warped, botCoordinates, ballCoordinates, (0,255,0), 3)
 
 #resize and show window with picture: warped
 warped = cv2.resize(warped, (600, 400))
-cv2.imshow("arrow", warped)
+cv2.imshow("Warped", warped)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
