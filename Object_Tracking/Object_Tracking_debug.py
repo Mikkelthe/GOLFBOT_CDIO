72c424b1f20ae39332e0f16cc8b0a1f98df85504
mikkelthe <s204107@student.dtu.dk>
2026-06-09 13:46:23 +0200

    Crossdetection, easier ignoring of taking new images

--- Object_Tracking/Object_Tracking_debug.py
+++ Object_Tracking/Object_Tracking_debug.py
@@ -15,22 +15,23 @@ import numpy as np
 from pathlib import Path
 
 if __name__ == "__main__":
-    i = 0
+    start_i = 22
+    i = start_i
     base_path = Path(__file__).resolve().parent
     output_folder = base_path.parent / "Warped_Images"
     output_folder.mkdir(exist_ok=True)
-
+    imagecount = 20
         
     # ---- Court settings ----
     WARP_W, WARP_H = 1200, 800
     COURT_W_CM, COURT_H_CM = 180.0, 120.0
-
-    videodevice = cv2.VideoCapture(1)
-    videodevice.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    videodevice.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    time.sleep(5)
+    if i < imagecount:
+        videodevice = cv2.VideoCapture(1)
+        videodevice.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        videodevice.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        time.sleep(5)
     start_time = time.time()
-    while i < 20:
+    while i < imagecount:
         i += 1
         if i%5 == 0:
             print("move")
@@ -47,9 +48,10 @@ if __name__ == "__main__":
 
     images_folder = base_path.parent / "Images"
     new_time = time.time()
-    videodevice.release()
-    print(time.time() - new_time)
-    print(time.time() - start_time)
+    if imagecount >= start_i:
+        videodevice.release()
+        print(time.time() - new_time)
+        print(time.time() - start_time)
 
     
 
