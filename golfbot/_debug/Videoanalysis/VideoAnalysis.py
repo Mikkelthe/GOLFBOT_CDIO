1bc02226f41b685f2b2d2ff5e893df3520a0cc4d
Christian Roar Weber <94703124+Baktwoone@users.noreply.github.com>
2026-06-25 08:50:23 +0200

    tiny tiny change

--- golfbot/_debug/Videoanalysis/VideoAnalysis.py
+++ golfbot/_debug/Videoanalysis/VideoAnalysis.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     WARP_W, WARP_H = 1500, 1000
     COURT_W_CM, COURT_H_CM = 170.0, 125.0
 
-    videodevice = cv2.VideoCapture(1,cv2.CAP_DSHOW)
+    videodevice = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     videodevice.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     videodevice.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
